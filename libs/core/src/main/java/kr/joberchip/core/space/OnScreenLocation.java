90346312200ff34f82afe035ef8144bea1280b7c
YongHo Shin <gdtknight@naver.com>
2023-09-17 18:14:38 +0900

    [libs-core] feat: OnScreenLocation 엔티티 클래스 작성 (#3)

---
+++ libs/core/src/main/java/kr/joberchip/core/space/OnScreenLocation.java
@@ -0,0 +1,20 @@
+package kr.joberchip.core.space;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import kr.joberchip.core.BaseEntity;
+
+@MappedSuperclass
+public abstract class OnScreenLocation extends BaseEntity {
+  @Column(name = "x_position")
+  private Integer x;
+
+  @Column(name = "y_position")
+  private Integer y;
+
+  @Column(name = "width")
+  private Integer width;
+
+  @Column(name = "height")
+  private Integer height;
+}
