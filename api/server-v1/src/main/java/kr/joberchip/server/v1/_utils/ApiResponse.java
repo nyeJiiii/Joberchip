1aee22c990540795c4b3b7e566895d85cc75c68d
YongHo Shin <gdtknight@naver.com>
2023-09-18 12:33:49 +0900

    [api-server-v1] feat: ApiResponse 유틸리티 클래스 추가 (#10)

---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/_utils/ApiResponse.java
@@ -0,0 +1,33 @@
+package kr.joberchip.server.v1._utils;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse {
+  public static <T> Result<T> success() {
+    return Result.of(HttpStatus.NO_CONTENT.value(), true, null, null);
+  }
+
+  public static <T> Result<T> success(T response) {
+    return Result.of(HttpStatus.OK.value(), true, response, null);
+  }
+
+  public static Result<Object> error(String message, HttpStatus status) {
+    return Result.of(status.value(), false, null, Error.of(message));
+  }
+
+  public record Result<T>(int status, boolean success, T response, Error error) {
+    public static <T> Result<T> of(int status, boolean success, T response, Error error) {
+      return new Result<T>(status, success, response, error);
+    }
+  }
+
+  public record Error(String message) {
+    public static Error of(String message) {
+      return new Error(message);
+    }
+  }
+}
