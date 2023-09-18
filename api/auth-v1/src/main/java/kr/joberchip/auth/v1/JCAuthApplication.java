190f2bf0460ee26ea6a8d076bf9ea4322bcf503c
HanHyeJi <hjhan.0621@gmail.com>
2023-09-18 15:13:04 +0900

    [api-auth-v1] feat: JCAuthApplication 생성 (#12)

---
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/JCAuthApplication.java
@@ -0,0 +1,11 @@
+package kr.joberchip.auth.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JCAuthApplication.class, args);
+    }
+}
