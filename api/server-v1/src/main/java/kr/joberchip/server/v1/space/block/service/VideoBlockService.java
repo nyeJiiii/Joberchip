c7af748fc03235dcce2a036fb2d95be5e3dc6e3d
YongHo Shin <gdtknight@naver.com>
2023-09-18 12:38:23 +0900

    [api-server-v1] feat: BlockService class 추가 - 미구현 상태 (#10)

---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/service/VideoBlockService.java
@@ -0,0 +1,8 @@
+package kr.joberchip.server.v1.space.block.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class VideoBlockService {}
