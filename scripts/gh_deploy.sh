b8fcaf8a485ba20d241fe24428c9d7d0a1f2f1d3
YongHo Shin <gdtknight@naver.com>
2023-09-19 01:05:46 +0900

    [root] fix: gh_deploy.sh 파일 오류 수정 (#13)

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -55,8 +55,8 @@ DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
 
 API_SERVER_LOG_PATH="/home/ubuntu/$PROJECT_NAME/api-server.log"
 API_SERVER_JAR_PATH="/home/ubuntu/$PROJECT_NAME/api/server-v1/build/libs/*.jar"
-API_SERVER_BUILD_JAR=$(ls "$API_SERVER_JAR_PATH")
-API_SERVER_JAR_NAME=$(basename "$API_SERVER_BUILD_JAR")
+API_SERVER_BUILD_JAR=$(ls $API_SERVER_JAR_PATH)
+API_SERVER_JAR_NAME=$(basename $API_SERVER_BUILD_JAR)
 
 echo "==== api server 배포 시작 : $(date +%c) ====" >> $DEPLOY_LOG_PATH
 
@@ -64,25 +64,25 @@ echo "> api-auth build 파일명 : $API_AUTH_JAR_NAME" >> $DEPLOY_LOG_PATH
 echo "> application build 파일명 : $API_SERVER_JAR_NAME" >> $DEPLOY_LOG_PATH
 
 echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
-cp "$API_SERVER_BUILD_JAR" "$DEPLOY_PATH"
+cp $API_SERVER_BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 동작중인 애플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
-API_SERVER_CURRENT_PID=$(pgrep -f "$API_SERVER_JAR_NAME")
+API_SERVER_CURRENT_PID=$(pgrep -f $API_SERVER_JAR_NAME)
 
-if [ -z "$API_SERVER_CURRENT_PID" ]
+if [ -z $API_SERVER_CURRENT_PID ]
 then
   echo "> 현재 동작중인 API SERVER 존재 X" >> $DEPLOY_LOG_PATH
 else
-  echo "> 현재 동작중인 API SERVER 존재 O" >> "$DEPLOY_LOG_PATH"
-  echo "> 현재 동작중인 API SERVER 강제 종료 진행" >> "$DEPLOY_LOG_PATH"
+  echo "> 현재 동작중인 API SERVER 존재 O" >> $DEPLOY_LOG_PATH
+  echo "> 현재 동작중인 API SERVER 강제 종료 진행" >> $DEPLOY_LOG_PATH
   echo "> kill -9 $API_SERVER_CURRENT_PID" >> $DEPLOY_LOG_PATH
-  kill -9 "$API_SERVER_CURRENT_PID"
+  kill -9 $API_SERVER_CURRENT_PID
 fi
 
 API_SERVER_DEPLOY_JAR=$DEPLOY_PATH$API_SERVER_JAR_NAME
 
 echo "> API_SERVER_DEPLOY_JAR 배포" >> $API_SERVER_LOG_PATH
-nohup java -Xms128m -Xmx196m -jar -Dspring.profiles.active=prod "$API_SERVER_DEPLOY_JAR" >> $API_SERVER_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+nohup java -Xms128m -Xmx196m -jar -Dspring.profiles.active=prod $API_SERVER_DEPLOY_JAR >> $API_SERVER_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
 sleep 3
 
