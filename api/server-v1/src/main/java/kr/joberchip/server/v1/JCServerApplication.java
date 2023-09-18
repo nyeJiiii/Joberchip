6185cf2fed6c61814c058eeb6fda98d2bc1ebf93
YongHo Shin <gdtknight@naver.com>
2023-09-18 12:30:39 +0900

    [api-server-v1] feat: JCServerApplication & test 구현 (#10)

---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/JCServerApplication.java
@@ -0,0 +1,11 @@
+package kr.joberchip.server.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(JCServerApplication.class, args);
+  }
+}
---
+++ api/server-v1/src/test/java/kr/joberchip/server/v1/JCServerApplicationTest.java
@@ -0,0 +1,13 @@
+package kr.joberchip.server.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class JCServerApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
