2ea171d9b21fbc831be98636aec3ac3774a5d23a
YongHo Shin <gdtknight@naver.com>
2023-09-18 12:36:28 +0900

    [api-server-v1] feat: dto class 추가 - 미구현 상태 (#10)

---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/dto/create/CreateTextBlock.java
@@ -0,0 +1,3 @@
+package kr.joberchip.server.v1.space.block.dto.create;
+
+public class CreateTextBlock {}
---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/dto/modify/ModifyLinkBlock.java
@@ -0,0 +1,3 @@
+package kr.joberchip.server.v1.space.block.dto.modify;
+
+public class ModifyLinkBlock {}
