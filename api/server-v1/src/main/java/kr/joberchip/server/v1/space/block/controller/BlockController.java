287f8075c90be790123a842d4ea1182995b211c2
YongHo Shin <gdtknight@naver.com>
2023-09-18 12:36:12 +0900

    [api-server-v1] feat: Controller class 추가 - 미구현 상태 (#10)

---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/controller/BlockController.java
@@ -0,0 +1,6 @@
+package kr.joberchip.server.v1.space.block.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlockController {}
---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/controller/TemplateBlockController.java
@@ -0,0 +1,8 @@
+package kr.joberchip.server.v1.space.block.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TemplateBlockController {}
