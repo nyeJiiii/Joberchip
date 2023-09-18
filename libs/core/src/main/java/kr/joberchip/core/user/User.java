330413d95460d85ccb310cd669abbb777a1f9a05
HanHyeJi <hjhan.0621@gmail.com>
2023-09-18 17:36:53 +0900

    [api-auth-v1] feat: User 클래스 > password 칼럼, @Getter 추가 (#12)

--- libs/core/src/main/java/kr/joberchip/core/user/User.java
+++ libs/core/src/main/java/kr/joberchip/core/user/User.java
@@ -3,10 +3,12 @@ package kr.joberchip.core.user;
 import javax.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "user_tb")
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class User {
@@ -18,4 +20,7 @@ public class User {
 
   @Column(nullable = false, unique = true)
   private String username;
+
+  @Column(nullable = false)
+  private String password;
 }
