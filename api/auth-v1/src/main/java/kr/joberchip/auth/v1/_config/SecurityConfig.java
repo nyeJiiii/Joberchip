263d551c319c59b243805cb22f950270d3126caf
HanHyeJi <hjhan.0621@gmail.com>
2023-09-20 05:53:55 +0900

    [api-auth-v1] feat: CustomSecurityFilterManager 구현 및 적용 (#32)

--- api/auth-v1/src/main/java/kr/joberchip/auth/v1/_config/SecurityConfig.java
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/_config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package kr.joberchip.auth.v1._config;
 
+import kr.joberchip.auth.v1.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,6 +21,15 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    public class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            builder.addFilter(new JwtAuthenticationFilter(authenticationManager));
+            super.configure(builder);
+        }
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -27,7 +38,8 @@ public class SecurityConfig {
             .and().cors().configurationSource(configurationSource())
             .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and().formLogin().disable()
-                  .httpBasic().disable();
+                  .httpBasic().disable()
+                  .apply(new CustomSecurityFilterManager());
         http.authorizeRequests()
                 .antMatchers("/user").authenticated()
                 .anyRequest().permitAll();
