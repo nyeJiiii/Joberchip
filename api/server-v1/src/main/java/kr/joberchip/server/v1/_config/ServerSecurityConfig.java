79c69237ad6c06b9e3cfaaffeccf1934d567c16f
YongHo Shin <gdtknight@naver.com>
2023-09-19 13:29:40 +0900

    [api-server-v1] feat: ServerSecurityConfig 설정 추가 (#20)
    - 개발 편의를 위해 모든 경로에 대한 요청 허용

--- api/server-v1/src/main/java/kr/joberchip/server/v1/_config/ServerSecurityConfig.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/_config/ServerSecurityConfig.java
@@ -1,6 +1,45 @@
 package kr.joberchip.server.v1._config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-public class ServerSecurityConfig {}
+public class ServerSecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf()
+        .disable()
+        .headers()
+        .frameOptions()
+        .sameOrigin()
+        .and()
+        .cors()
+        .configurationSource(configurationSource())
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    http.httpBasic().disable().formLogin().disable();
+    http.authorizeRequests().anyRequest().permitAll();
+
+    return http.build();
+  }
+
+  public CorsConfigurationSource configurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.addAllowedHeader("*");
+    configuration.addAllowedMethod("*");
+    configuration.addAllowedOriginPattern("*");
+    configuration.setAllowCredentials(true);
+    configuration.addExposedHeader("Authorization");
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+}
