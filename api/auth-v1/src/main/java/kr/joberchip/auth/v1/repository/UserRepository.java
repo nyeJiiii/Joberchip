0eced43f83b24a5523d1731578c5ebc50a998c27
YongHo Shin <gdtknight@naver.com>
2023-09-19 13:35:51 +0900

    [api-auth-v1] feat: UserRepository - @Repository 어노테이션 추가 (#20)

--- api/auth-v1/src/main/java/kr/joberchip/auth/v1/repository/UserRepository.java
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package kr.joberchip.auth.v1.repository;
 
+import java.util.Optional;
 import kr.joberchip.core.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(@Param("username") String username);
+  Optional<User> findByUsername(@Param("username") String username);
 }
