7be2f8cff127a9cd238d0d145e0414f8e3b32ecc
HanHyeJi <hjhan.0621@gmail.com>
2023-09-20 05:52:56 +0900

    [api-auth-v1] feat: authenticationManager() 생성 (#32)

--- api/auth-v1/src/main/java/kr/joberchip/auth/v1/_config/SecurityConfig.java
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/_config/SecurityConfig.java
@@ -2,6 +2,8 @@ package kr.joberchip.auth.v1._config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +13,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
