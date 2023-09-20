21bcdf174be131fe7500b4cb3d8fb0ce8c50642c
HanHyeJi <hjhan.0621@gmail.com>
2023-09-20 14:25:59 +0900

    [api-auth-v1] feat: UserController > join()-회원가입 로직 구현 (#35)

--- api/auth-v1/src/main/java/kr/joberchip/auth/v1/controller/UserController.java
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/controller/UserController.java
@@ -1,10 +1,23 @@
 package kr.joberchip.auth.v1.controller;
 
+import kr.joberchip.auth.v1._utils.ApiResponse;
+import kr.joberchip.auth.v1.dto.UserRequest;
+import kr.joberchip.auth.v1.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
 @RestController
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/")
     public String home() {
@@ -16,8 +29,9 @@ public class UserController {
         return "유저페이지";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "로그인페이지";
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody @Valid UserRequest newUser, Errors errors) {
+        userService.join(newUser);
+        return ResponseEntity.ok().body(ApiResponse.success("회원가입 성공"));
     }
 }
