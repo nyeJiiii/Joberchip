dfeb3262ddb2d87046392095bb8f28801ff5957f
YongHo Shin <gdtknight@naver.com>
2023-09-20 21:34:28 +0900

    [libs-core] feat: BaseEntity - Jpa Auditing 적용  (#38)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/_config/ServerJpaConfig.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/_config/ServerJpaConfig.java
@@ -1,12 +1,31 @@
 package kr.joberchip.server.v1._config;
 
+import java.util.Optional;
+import kr.joberchip.server.v1._config.security.CustomUserDetails;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EntityScan(basePackages = {"kr.joberchip.core"})
-@EnableJpaRepositories(basePackages = {"kr.joberchip.server.v1.space"})
+@EnableJpaRepositories(
+    basePackages = {"kr.joberchip.server.v1.space", "kr.joberchip.server.v1.user"})
 @EnableJpaAuditing
-public class ServerJpaConfig {}
+public class ServerJpaConfig {
+  @Bean
+  public AuditorAware<String> auditorAware() {
+    return () ->
+        Optional.ofNullable(SecurityContextHolder.getContext())
+            .map(SecurityContext::getAuthentication)
+            .filter(Authentication::isAuthenticated)
+            .map(Authentication::getPrincipal)
+            .map(CustomUserDetails.class::cast)
+            .map(CustomUserDetails::getUsername);
+  }
+}
