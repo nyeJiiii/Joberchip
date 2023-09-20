4c80206b102a7ccca153609af07856a379ba1c1c
HanHyeJi <hjhan.0621@gmail.com>
2023-09-20 16:51:52 +0900

    [api-auth-v1] feat: UserController > login()-로그인 로직 구현 (#35)
    로그인 성공 후 ResponseEntity에
    Header에는 JWT, Body에는 userId와 username 포함시킴

--- api/auth-v1/src/main/java/kr/joberchip/auth/v1/controller/UserController.java
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/controller/UserController.java
@@ -2,10 +2,12 @@ package kr.joberchip.auth.v1.controller;
 
 import kr.joberchip.auth.v1._utils.ApiResponse;
 import kr.joberchip.auth.v1.dto.UserRequest;
+import kr.joberchip.auth.v1.dto.UserResponse;
+import kr.joberchip.auth.v1.security.JwtTokenProvider;
 import kr.joberchip.auth.v1.service.UserService;
+import kr.joberchip.core.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,8 +32,21 @@ public class UserController {
     }
 
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody @Valid UserRequest newUser, Errors errors) {
+    public ResponseEntity<?> join(@RequestBody @Valid UserRequest newUser) {
+        // TODO @Valid 유효성검사 Exception (Null 값 들어왔을 때)
+
         userService.join(newUser);
         return ResponseEntity.ok().body(ApiResponse.success("회원가입 성공"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid UserRequest loginUser) {
+        // TODO @Valid 유효성검사 Exception (Null 값 들어왔을 때)
+
+        User athurizedUser = userService.login(loginUser);
+        String token = JwtTokenProvider.create(athurizedUser);
+        return ResponseEntity.ok()
+                .header("Authorization", token)
+                .body(ApiResponse.success(new UserResponse(athurizedUser)));
     }
 }
