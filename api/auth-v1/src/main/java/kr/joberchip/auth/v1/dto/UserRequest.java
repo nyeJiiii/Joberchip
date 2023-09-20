38671813304e402e8b1a78919e1d0880688b2d51
HanHyeJi <hjhan.0621@gmail.com>
2023-09-20 14:19:30 +0900

    [api-auth-v1] feat: UserRequest(DTO) 생성 (#35)

---
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/dto/UserRequest.java
@@ -0,0 +1,27 @@
+package kr.joberchip.auth.v1.dto;
+
+import kr.joberchip.core.user.User;
+import lombok.Getter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+public class UserRequest {
+
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+
+    public void encodePassword (String password) {
+        this.password = password;
+    }
+
+    public User toEntity () {
+        return User.builder()
+                .username(username)
+                .password(password)
+                .build();
+    }
+
+}
