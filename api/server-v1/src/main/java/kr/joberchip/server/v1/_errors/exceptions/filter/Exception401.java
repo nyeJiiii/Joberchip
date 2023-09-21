a8fd9bf20f3ab6f2a14f2d3cb50a6e9f1fcf46e9
YongHo Shin <gdtknight@naver.com>
2023-09-21 12:04:42 +0900

    [api-server-v1] feat: 인증 필터 관련 예외 클래스 추가 (#38)

---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/_errors/exceptions/filter/Exception401.java
@@ -0,0 +1,21 @@
+package kr.joberchip.server.v1._errors.exceptions.filter;
+
+import kr.joberchip.server.v1._utils.ApiResponse;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class Exception401 extends RuntimeException {
+
+  public Exception401(String message) {
+    super(message);
+  }
+
+  public ApiResponse.Result<Object> body() {
+    return ApiResponse.error(getMessage(), status());
+  }
+
+  public HttpStatus status() {
+    return HttpStatus.UNAUTHORIZED;
+  }
+}
