31588f9c2b9616a0366896f0a315c8936b1317a6
YongHo Shin <gdtknight@naver.com>
2023-09-21 12:33:05 +0900

    [api-server-v1] update: ServerSecurityConfig - API 테스트 위한 임시 설정 추가 (#38)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/_config/ServerSecurityConfig.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/_config/ServerSecurityConfig.java
@@ -1,8 +1,17 @@
 package kr.joberchip.server.v1._config;
 
+import kr.joberchip.server.v1._config.security.JwtAuthenticationFilter;
+import kr.joberchip.server.v1._errors.ErrorMessage;
+import kr.joberchip.server.v1._errors.exceptions.filter.Exception401;
+import kr.joberchip.server.v1._errors.exceptions.filter.Exception403;
+import kr.joberchip.server.v1._utils.FilterResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,9 +19,16 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 public class ServerSecurityConfig {
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf()
         .disable()
         .headers()
@@ -23,23 +39,58 @@ public class ServerSecurityConfig {
         .configurationSource(configurationSource())
         .and()
         .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-    http.httpBasic().disable().formLogin().disable();
-    http.authorizeRequests().anyRequest().permitAll();
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .formLogin()
+        .disable()
+        .httpBasic()
+        .disable()
+        .apply(new SecurityFilterManager())
+        .and()
+        .exceptionHandling()
+        .authenticationEntryPoint(
+            (request, response, authException) ->
+                FilterResponse.unAuthorized(response, new Exception401(ErrorMessage.UN_AUTHORIZED)))
+        .and()
+        .exceptionHandling()
+        .accessDeniedHandler(
+            (request, response, accessDeniedException) ->
+                FilterResponse.forbidden(response, new Exception403(ErrorMessage.FORBIDDEN)))
+        .and()
+        .authorizeRequests(
+            expressionInterceptUrlRegistry ->
+                expressionInterceptUrlRegistry
+                    .antMatchers("/v1/user/testTokens")
+                    .permitAll()
+                    .antMatchers("/v1/**")
+                    .authenticated());
 
     return http.build();
   }
 
-  public CorsConfigurationSource configurationSource() {
+  private CorsConfigurationSource configurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.addAllowedHeader("*");
     configuration.addAllowedMethod("*");
     configuration.addAllowedOriginPattern("*");
     configuration.setAllowCredentials(true);
     configuration.addExposedHeader("Authorization");
+
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
+
     return source;
+  }
+
+  public static class SecurityFilterManager
+      extends AbstractHttpConfigurer<SecurityFilterManager, HttpSecurity> {
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+      AuthenticationManager authenticationManager =
+          builder.getSharedObject(AuthenticationManager.class);
+      builder.addFilter(new JwtAuthenticationFilter(authenticationManager));
+      super.configure(builder);
+    }
   }
 }
