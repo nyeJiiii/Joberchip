57f4d07361f9fd7ff92a1514f8604a203fd6a2ab
YongHo Shin <gdtknight@naver.com>
2023-09-25 14:27:28 +0900

    [libs-core] update: OnScreenLocation - 접근제어자 protected 변경, @Getter @Setter 임시로 추가 (#43)

--- libs/core/src/main/java/kr/joberchip/core/share/OnScreenLocation.java
+++ libs/core/src/main/java/kr/joberchip/core/share/OnScreenLocation.java
@@ -3,18 +3,22 @@ package kr.joberchip.core.share;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import kr.joberchip.core.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 @MappedSuperclass
+@Getter
+@Setter
 public abstract class OnScreenLocation extends BaseEntity {
-  @Column(name = "x_position")
-  private Integer x;
+  @Column(name = "x_pos")
+  protected Integer x;
 
-  @Column(name = "y_position")
-  private Integer y;
+  @Column(name = "y_pos")
+  protected Integer y;
 
   @Column(name = "width")
-  private Integer width;
+  protected Integer width;
 
   @Column(name = "height")
-  private Integer height;
+  protected Integer height;
 }
