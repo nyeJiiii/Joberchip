70d578d40de183afed4c9edb9dbf202721620688
HanHyeJi <hjhan.0621@gmail.com>
2023-09-25 20:56:32 +0900

    [auth-sever-v1] feat: CreateMapBlock > toEntity() 생성 (#51)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/dto/create/CreateMapBlock.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/dto/create/CreateMapBlock.java
@@ -1,5 +1,6 @@
 package kr.joberchip.server.v1.share.block.dto.create;
 
+import kr.joberchip.core.share.block.MapBlock;
 import lombok.Getter;
 
 @Getter
@@ -8,5 +9,13 @@ public class CreateMapBlock {
     public String address;
     public Double latitude;
     public Double longitude;
+
+    public MapBlock toEntity() {
+        return MapBlock.builder()
+                .address(address)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build();
+    }
 
 }
