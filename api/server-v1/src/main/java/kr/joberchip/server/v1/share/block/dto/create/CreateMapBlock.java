80c7274fb3437cb0efdc33e1c32614f7183cd05f
HanHyeJi <hjhan.0621@gmail.com>
2023-09-25 20:48:24 +0900

    [auth-sever-v1] feat: CreateMapBlock DTO 생성 (#51)

---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/dto/create/CreateMapBlock.java
@@ -0,0 +1,12 @@
+package kr.joberchip.server.v1.share.block.dto.create;
+
+import lombok.Getter;
+
+@Getter
+public class CreateMapBlock {
+
+    public String address;
+    public Double latitude;
+    public Double longitude;
+
+}
