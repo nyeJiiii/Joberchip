bfae393faf1e22ba78be9ea7039f805f0e8d1be9
HanHyeJi <hjhan.0621@gmail.com>
2023-09-25 13:15:52 +0900

    [api-auth-v1] feat: ExceptionHandler 클래스 (#40)

---
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/errors/GlobalValidationHandler.java
@@ -0,0 +1,36 @@
+package kr.joberchip.auth.v1.errors;
+
+import kr.joberchip.auth.v1.errors.exception.Exception400;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+@Aspect
+@Component
+public class GlobalValidationHandler {
+
+  @Pointcut(value = "@annotation(org.springframework.web.bind.annotation.PostMapping)")
+  public void postMapping() {}
+
+  @Pointcut(value = "@annotation(org.springframework.web.bind.annotation.PutMapping)")
+  public void putMapping() {}
+
+  @Before("postMapping() || putMapping()")
+  public void validationAdvice(JoinPoint jp) {
+    Object[] args = jp.getArgs();
+    for (Object arg : args) {
+      if (arg instanceof Errors) {
+        Errors errors = (Errors) arg;
+
+        if (errors.hasErrors()) {
+          throw new Exception400(
+              errors.getFieldErrors().get(0).getField(),
+              errors.getFieldErrors().get(0).getDefaultMessage());
+        }
+      }
+    }
+  }
+}
