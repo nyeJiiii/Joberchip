b7bfcdc36cdde3f704940004e3861dccddb38233
HanHyeJi <hjhan.0621@gmail.com>
2023-09-25 16:15:21 +0900

    [api-auth-v1] feat: UserService > 로그인 시 비밀번호 검증 로직 추가 (#40)

--- api/auth-v1/src/main/java/kr/joberchip/auth/v1/user/service/UserService.java
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/user/service/UserService.java
@@ -8,12 +8,13 @@ import kr.joberchip.auth.v1.user.repository.UserRepository;
 import kr.joberchip.auth.v1.security.JwtTokenProvider;
 import kr.joberchip.core.user.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -37,16 +38,27 @@ public class UserService {
 
     @Transactional
     public String login(UserRequest loginUser) {
-        Optional<User> userOptional = findUser(loginUser);
+        Optional<User> userOptional = checkUser(loginUser);
         User user = userOptional.get();
         return JwtTokenProvider.create(user);
     }
 
-    private Optional<User> findUser(UserRequest loginUser) {
+    private Optional<User> checkUser(UserRequest loginUser) {
         Optional<User> userOptional = userRepository.findByUsername(loginUser.getUsername());
         if (userOptional.isEmpty()) {
+            log.info("ID를 찾을 수 없음");
             throw new UserNotFoundException(ErrorMessage.USER_NOT_FOUND);
         }
-        return userOptional;
+
+        return checkPassword(loginUser, userOptional);
+    }
+
+    private Optional<User> checkPassword(UserRequest loginUser, Optional<User> userOptional) {
+        if(!passwordEncoder.matches(loginUser.getPassword(), userOptional.get().getPassword())) {
+            log.info("비밀번호가 일치하지 않음");
+            throw new UserNotFoundException(ErrorMessage.USER_NOT_FOUND);
+        } else {
+            return userOptional;
+        }
     }
 }
