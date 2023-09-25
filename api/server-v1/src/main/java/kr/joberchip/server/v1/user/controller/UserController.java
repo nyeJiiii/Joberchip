d91097fe6ea5676d4e1452b628eccf1705361424
YongHo Shin <gdtknight@naver.com>
2023-09-25 14:45:53 +0900

    [api-server-v1] update: UserController - 임시 토큰 발급 API 응답 형식 수정 (#47)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/user/controller/UserController.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/user/controller/UserController.java
@@ -1,12 +1,13 @@
 package kr.joberchip.server.v1.user.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import kr.joberchip.server.v1._config.security.JwtProvider;
 import kr.joberchip.server.v1._utils.ApiResponse;
 import kr.joberchip.server.v1.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -17,13 +18,19 @@ public class UserController {
   private final UserRepository userRepository;
 
   @GetMapping("/testTokens")
-  public ApiResponse.Result<Map<String, String>> generateTestTokens() {
-    Map<String, String> usernameAndToken = new HashMap<>();
+  public ResponseEntity<ApiResponse.Result<List<TokenResponse>>> generateTestTokens() {
+    List<TokenResponse> tokens = new ArrayList<>();
 
-    userRepository
-        .findAll()
-        .forEach(user -> usernameAndToken.put(user.getUsername(), JwtProvider.create(user)));
+    userRepository.findAll().stream()
+        .map(user -> TokenResponse.of(user.getUsername(), JwtProvider.create(user)))
+        .forEach(tokens::add);
 
-    return ApiResponse.success(usernameAndToken);
+    return ResponseEntity.ok(ApiResponse.success(tokens));
+  }
+
+  public record TokenResponse(String username, String token) {
+    public static TokenResponse of(String username, String token) {
+      return new TokenResponse(username, token);
+    }
   }
 }
