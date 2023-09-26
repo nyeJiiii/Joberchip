e3fa415c54d2711cdc56686e39c0bfa98a82620a
HanHyeJi <hjhan.0621@gmail.com>
2023-09-26 16:00:23 +0900

    [auth-sever-v1] feat: MapBlockRepository > 지도블럭 수정 쿼리 구현 (#51)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/repository/MapBlockRepository.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/repository/MapBlockRepository.java
@@ -2,7 +2,23 @@ package kr.joberchip.server.v1.share.block.repository;
 
 import kr.joberchip.core.share.block.MapBlock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
-public interface MapBlockRepository extends JpaRepository<MapBlock, UUID> {}
+public interface MapBlockRepository extends JpaRepository<MapBlock, UUID> {
+
+    @Modifying
+    @Query(value =
+            "UPDATE MapBlock " +
+            "SET address = :newAddress, latitude = :newLatitude, longitude = :newLongitude " +
+            "WHERE objectId = :id")
+    void updateById(
+            @Param("newAddress") String newAddress,
+            @Param("newLatitude") Double newLatitude,
+            @Param("newLongitude") Double newLongitude,
+            @Param("id") UUID id);
+
+}
