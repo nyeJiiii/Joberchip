b3f5f0b1067c4c745775bd63367ea270a08203ae
YongHo Shin <gdtknight@naver.com>
2023-09-27 11:03:02 +0900

    [api-server-v1] feat: ParticipatingInfo - 스페이스 목록 조회시 메인 페이지 타이틀을 함께 전달하도록 수정

--- api/server-v1/src/main/java/kr/joberchip/server/v1/space/dto/ParticipatingInfo.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/dto/ParticipatingInfo.java
@@ -5,12 +5,13 @@ import kr.joberchip.core.space.types.ParticipationType;
 import kr.joberchip.core.user.SpaceUserInfo;
 
 public record ParticipatingInfo(
-    UUID spaceId, UUID mainPageId, ParticipationType participationType) {
+    UUID spaceId, UUID mainPageId, String mainPageTitle, ParticipationType participationType) {
 
   public static ParticipatingInfo fromEntity(SpaceUserInfo spaceUserInfo) {
     return new ParticipatingInfo(
         spaceUserInfo.getSpace().getSpaceId(),
         spaceUserInfo.getSpace().getMainPage().getPageId(),
+        spaceUserInfo.getSpace().getMainPage().getTitle(),
         spaceUserInfo.getParticipationType());
   }
 }
