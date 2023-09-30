c926cf5ca97807920d00ef3070b32e4749cff276
HanHyeJi <hjhan.0621@gmail.com>
2023-09-30 17:38:08 +0900

    [server] update: data.sql > 유저 생성시 nickname 값 추가 (#63)

--- api/server-v1/src/main/resources/test/data.sql
+++ api/server-v1/src/main/resources/test/data.sql
@@ -2,16 +2,16 @@
   user_tb dummy
   password = '0000'
 */
-insert into user_tb (username, password, user_roles)
-values ('user1', '{bcrypt}$2a$10$B9YJT9AtusUZJAnmgznu1OX1kI.8TDizhfCPYCUQMOWat6K1s4oM.', 'ROLE_USER');
-insert into user_tb (username, password, user_roles)
-values ('user2', '{bcrypt}$2a$10$s0CMkutBLP.k5mz4rI5T/e0tOj9WWUDXBTYqsM4AQ3NS/30OphvIa', 'ROLE_USER');
-insert into user_tb (username, password, user_roles)
-values ('user3', '{bcrypt}$2a$10$85RcQmwBZQNLvI31sbGiN.KbtDIGxFnBuKkRNXEbFh6PpMdv8vY3.', 'ROLE_USER');
-insert into user_tb (username, password, user_roles)
-values ('user4', '{bcrypt}$2a$10$TU81VtR4POmoLoHyZVJCme5NwNEMfHKYZmKpiJdqBRNdF0qVzRwUS', 'ROLE_USER');
-insert into user_tb (username, password, user_roles)
-values ('user5', '{bcrypt}$2a$10$YC.lqsJc6stMpZu.Hzfctu0oEPCrRZmJsZ7ua8He6r6RbN2i4jUZO', 'ROLE_USER');
+insert into user_tb (username, password, nickname, user_roles)
+values ('user1', '{bcrypt}$2a$10$B9YJT9AtusUZJAnmgznu1OX1kI.8TDizhfCPYCUQMOWat6K1s4oM.', 'user1', 'ROLE_USER');
+insert into user_tb (username, password, nickname, user_roles)
+values ('user2', '{bcrypt}$2a$10$s0CMkutBLP.k5mz4rI5T/e0tOj9WWUDXBTYqsM4AQ3NS/30OphvIa', 'user2', 'ROLE_USER');
+insert into user_tb (username, password, nickname, user_roles)
+values ('user3', '{bcrypt}$2a$10$85RcQmwBZQNLvI31sbGiN.KbtDIGxFnBuKkRNXEbFh6PpMdv8vY3.', 'user3', 'ROLE_USER');
+insert into user_tb (username, password, nickname, user_roles)
+values ('user4', '{bcrypt}$2a$10$TU81VtR4POmoLoHyZVJCme5NwNEMfHKYZmKpiJdqBRNdF0qVzRwUS', 'user4', 'ROLE_USER');
+insert into user_tb (username, password, nickname, user_roles)
+values ('user5', '{bcrypt}$2a$10$YC.lqsJc6stMpZu.Hzfctu0oEPCrRZmJsZ7ua8He6r6RbN2i4jUZO', 'user5', 'ROLE_USER');
 
 
 /*
