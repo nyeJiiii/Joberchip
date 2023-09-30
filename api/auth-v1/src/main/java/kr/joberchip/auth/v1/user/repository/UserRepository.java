8dd333cf40509f5f10d08fce5a618f7730de7b31
HanHyeJi <hjhan.0621@gmail.com>
2023-09-30 17:40:05 +0900

    [auth] feat: UserRepository > nickname 업데이트 쿼리 구현(#63)

--- api/auth-v1/src/main/java/kr/joberchip/auth/v1/user/repository/UserRepository.java
+++ api/auth-v1/src/main/java/kr/joberchip/auth/v1/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package kr.joberchip.auth.v1.user.repository;
 import java.util.Optional;
 import kr.joberchip.core.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,8 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByUsername(@Param("username") String username);
+
+  @Modifying
+  @Query(value = "UPDATE User SET nickname = :newNickname WHERE userId = :userId")
+  void updateNicknameById(@Param("newNickname") String newNickname, @Param("userId") Long userId);
 }
