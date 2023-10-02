9381abda46f1c8bc7b241e67b0879fbfcd324eaa
YongHo Shin <gdtknight@naver.com>
2023-10-02 20:55:15 +0900

    [server] remove: api-auth-v1 모듈 삭제 (통합 완료)

--- api/auth-v1/src/main/java/kr/joberchip/auth/v1/errors/exception/DuplicatedUsernameException.java
+++
@@ -1,7 +0,0 @@
-package kr.joberchip.auth.v1.errors.exception;
-
-public class DuplicatedUsernameException extends RuntimeException{
-    public DuplicatedUsernameException(String message) {
-        super(message);
-    }
-}
--- api/auth-v1/src/main/java/kr/joberchip/auth/v1/errors/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package kr.joberchip.auth.v1.errors.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
