67095127c672b8ac9299a581e10f79735fb71440
HanHyeJi <hjhan.0621@gmail.com>
2023-10-02 18:49:30 +0900

    [server] feat: TextBlockService > createTextBlock 구현 (#68)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/service/TextBlockService.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/service/TextBlockService.java
@@ -1,11 +1,12 @@
 package kr.joberchip.server.v1.share.block.service;
 
 import java.util.UUID;
-import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+
 import kr.joberchip.core.share.block.TextBlock;
 import kr.joberchip.core.share.page.SharePage;
+import kr.joberchip.server.v1._errors.ErrorMessage;
 import kr.joberchip.server.v1.share.block.controller.dto.TextBlockDTO;
-import kr.joberchip.server.v1.share.block.controller.dto.TextBlockResponse;
 import kr.joberchip.server.v1.share.block.repository.TextBlockRepository;
 import kr.joberchip.server.v1.share.page.repository.SharePageRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,17 +22,21 @@ public class TextBlockService {
   private final TextBlockRepository textBlockRepository;
 
   @Transactional
-  public TextBlockResponse createTextBlock(UUID pageId, TextBlockDTO textBlockDTO) {
-    TextBlock newTextBlock = textBlockDTO.toEntity();
+  public void createTextBlock(UUID pageId, TextBlockDTO.Create newTextBlock) {
+    SharePage parentPage = isPage(pageId);
+    TextBlock savedTextBlock =
+            textBlockRepository.save(
+                    newTextBlock.setEntity(new TextBlock())
+            );
+    parentPage.addTextBlock(savedTextBlock);
+    log.info("UUID of NEW MAP BLOCK: " + savedTextBlock.getTextBlockId());
+  }
 
-    textBlockRepository.save(newTextBlock);
-
-    SharePage parentPage =
-        sharePageRepository.findById(pageId).orElseThrow(EntityExistsException::new);
-
-    parentPage.addTextBlock(newTextBlock);
-
-    return TextBlockResponse.fromEntity(newTextBlock);
+  private SharePage isPage(UUID pageId) {
+    return sharePageRepository.findById(pageId).orElseThrow(() -> {
+      log.error("존재하지 않는 pageID - pageId: {}", pageId);
+      throw new EntityNotFoundException(ErrorMessage.ENTITY_NOT_FOUND);
+    });
   }
 
   public void modifyTextBlock(UUID pageId, UUID blockId, TextBlockDTO textBlockDTO) {
