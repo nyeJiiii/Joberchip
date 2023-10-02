f98bde63a1cda0ac28f2325c9dd15560ec0a2f5f
HanHyeJi <hjhan.0621@gmail.com>
2023-10-02 15:25:36 +0900

    [server] feat: MapBlockDTO > Create, Modify DTO 별도 생성 (#66)

---
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/controller/dto/MapBlockDTO.java
@@ -0,0 +1,48 @@
+package kr.joberchip.server.v1.share.block.controller.dto;
+
+import kr.joberchip.core.share.block.MapBlock;
+import kr.joberchip.server.v1._errors.ErrorMessage;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+
+
+public class MapBlockDTO {
+
+    @Getter
+    public class Create {
+        @NotNull(message = ErrorMessage.NOT_EMPTY)
+        private String address;
+        @NotNull(message = ErrorMessage.NOT_EMPTY)
+        private Double latitude;
+        @NotNull(message = ErrorMessage.NOT_EMPTY)
+        private Double longitude;
+        @NotNull(message = ErrorMessage.NOT_EMPTY)
+        private Integer x;
+        @NotNull(message = ErrorMessage.NOT_EMPTY)
+        private Integer y;
+        @NotNull(message = ErrorMessage.NOT_EMPTY)
+        private Integer height;
+        @NotNull(message = ErrorMessage.NOT_EMPTY)
+        private Integer width;
+
+        public MapBlock setEntity (MapBlock mapBlock){
+            mapBlock.setAddress(this.getAddress());
+            mapBlock.setLatitude(this.getLatitude());
+            mapBlock.setLongitude(this.getLongitude());
+            mapBlock.setX(this.getX());
+            mapBlock.setY(this.getY());
+            mapBlock.setHeight(this.getHeight());
+            mapBlock.setWidth(this.getWidth());
+            return mapBlock;
+        }
+    }
+
+    @Getter
+    public class Modify {
+        private String address;
+        private Double latitude;
+        private Double longitude;
+    }
+
+}
