8c19f42d4f9ecbb69e540f5c0f891053d3d385b5
HanHyeJi <hjhan.0621@gmail.com>
2023-10-04 22:11:37 +0900

    [server] update: @NotNull 유효성검사 삭제

--- api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/LinkBlockRequestDTO.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/LinkBlockRequestDTO.java
@@ -1,19 +1,17 @@
 package kr.joberchip.server.v1.block.controller.dto;
 
 import java.util.UUID;
-import javax.validation.constraints.NotNull;
 import kr.joberchip.core.block.LinkBlock;
-import kr.joberchip.server.v1._errors.ErrorMessage;
 
 public record LinkBlockRequestDTO(
     UUID pageId,
     String title,
     String description,
-    @NotNull(message = ErrorMessage.NOT_EMPTY) String link,
-    @NotNull(message = ErrorMessage.NOT_EMPTY) Integer x,
-    @NotNull(message = ErrorMessage.NOT_EMPTY) Integer y,
-    @NotNull(message = ErrorMessage.NOT_EMPTY) Integer w,
-    @NotNull(message = ErrorMessage.NOT_EMPTY) Integer h) {
+    String link,
+    Integer x,
+    Integer y,
+    Integer w,
+    Integer h) {
 
   public static LinkBlockRequestDTO of(UUID pageId, String title, String description, String link) {
     return new LinkBlockRequestDTO(pageId, title, description, link, 0, 0, 2, 2);
