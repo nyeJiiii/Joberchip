4c42b3b1336f2c094536436fe5a17110aecdacf6
YongHo Shin <gdtknight@naver.com>
2023-10-04 11:48:47 +0900

    [api-server-v1] feat: S3StorageConfig - S3 이용을 위한 Configuration class 설정

--- api/server-v1/src/main/java/kr/joberchip/server/v1/_config/ServerStorageConfig.java
+++
@@ -1,9 +0,0 @@
-package kr.joberchip.server.v1._config;
-
-import kr.joberchip.server.v1.storage.properties.StorageProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationPropertiesScan(basePackageClasses = {StorageProperties.class})
-public class ServerStorageConfig {}
