12b3dbbc4cc8442cba22e9b91ee804728d7ecc21
YongHo Shin <gdtknight@naver.com>
2023-10-05 20:31:31 +0900

    [api-server-v1] fix: TextBlockDTO - @RequestParam 제거

--- api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/TextBlockDTO.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/TextBlockDTO.java
@@ -1,15 +1,9 @@
 package kr.joberchip.server.v1.block.controller.dto;
 
 import kr.joberchip.core.block.TextBlock;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public record TextBlockDTO(
-    @RequestParam String content,
-    @RequestParam Integer x,
-    @RequestParam Integer y,
-    @RequestParam Integer w,
-    @RequestParam Integer h,
-    @RequestParam Boolean visible) {
+    String content, Integer x, Integer y, Integer w, Integer h, Boolean visible) {
   public TextBlock toEntity() {
     return TextBlock.of(content, x, y, w, h);
   }
