39725a26221a20a27c01671193730bf40a468346
YongHo Shin <gdtknight@naver.com>
2023-10-05 20:50:13 +0900

    [api-server-v1] update: TextBlockDTO - @Lob 어노테이션 추가

--- api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/TextBlockDTO.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/TextBlockDTO.java
@@ -1,9 +1,10 @@
 package kr.joberchip.server.v1.block.controller.dto;
 
+import javax.persistence.Lob;
 import kr.joberchip.core.block.TextBlock;
 
 public record TextBlockDTO(
-    String content, Integer x, Integer y, Integer w, Integer h, Boolean visible) {
+    @Lob String content, Integer x, Integer y, Integer w, Integer h, Boolean visible) {
   public TextBlock toEntity() {
     return TextBlock.of(content, x, y, w, h);
   }
