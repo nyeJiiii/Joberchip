96dd6a9a85c37007a3e45539e9f1857ca27affb0
YongHo Shin <gdtknight@naver.com>
2023-10-06 11:40:04 +0900

    [api-server-v1] update: Spring Security Debug Log - off

--- api/server-v1/src/main/java/kr/joberchip/server/v1/_config/ServerSecurityConfig.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/_config/ServerSecurityConfig.java
@@ -22,7 +22,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class ServerSecurityConfig {
 
   @Bean
